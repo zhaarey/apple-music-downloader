086fe7e8bff72b64cd7215a2a9a361d27868fb1b
itouakirai <85016486+itouakirai@users.noreply.github.com>
2024-04-24 17:55:46 +0800

    Update main_atmos.go

--- main_atmos.go
+++ main_atmos.go
@@ -1176,7 +1176,6 @@ func rip(albumId string, token string, storefront string, userToken string) erro
 			"tool=",
 			fmt.Sprintf("lyrics=%s", lrc),
 			fmt.Sprintf("cover=%s/cover.jpg", sanAlbumFolder),
-			fmt.Sprintf("album=%s", meta.Data[0].Attributes.Name),
 			fmt.Sprintf("title=%s", meta.Data[0].Relationships.Tracks.Data[index].Attributes.Name),
 			fmt.Sprintf("album=%s", meta.Data[0].Attributes.Name),
 			fmt.Sprintf("artist=%s", meta.Data[0].Relationships.Tracks.Data[index].Attributes.ArtistName),
